502c686550d3db713090b05d0ddae8d3816a57b0
Glenn Halls <gjhalls@gmail.com>
2023-06-08 14:40:59 +1000

    Lambda practice trickOrTreat

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,43 @@
-//Lambda functions and expressions
+// Training: Lambda Functions: Trick or Treat
 
 fun main() {
-    val testLambda = helloRepeat
-    testLambda(5)
+
+    val coins: (Int) -> String = { "$it quarters" }
+    val cupcake: (Int) -> String = { "Have a cupcake!" }
+
+
+    println(testOutput())
+    val treatFunction = trickOrTreat(false, coins)
+    var trickFunction = trickOrTreat(true, cupcake)
+
+    treatFunction()
+    trickFunction()
+
+    trickOrTreat(false, { "$it quarters" })()
+    trickOrTreat(false) { "$it quarters" }()
+    trickFunction = trickOrTreat(true, null)
+    trickFunction()
 }
 
-val helloRepeat: (Int) -> Unit = { repeat(it) {println("hello ")} }
+fun testOutput(): String {
+    return "output"
+}
+
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
+    if (isTrick) {
+        return trick
+    } else {
+        if (extraTreat != null) {
+            println(extraTreat(5))
+        }
+        return treat
+    }
+}
+
+ val trick = {
+     println("No treats!")
+ }
+
+val treat: () -> Unit = {
+    println("Have a treat!")
+}
