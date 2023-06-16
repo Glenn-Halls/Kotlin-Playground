7087d6c25c8ac072c7f39e6539b78c0a60a3f363
Glenn Halls <gjhalls@gmail.com>
2023-06-16 16:27:43 +1000

    map()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,28 @@
+class Cookie(
+    val name: String,
+    val softBakes: Boolean,
+    val hasFilling: Boolean,
+    val price: Double
+)
+
 fun main() {
-    val solarSystem = mutableMapOf(
-        "Mercury" to 0,
-        "Venus" to 0,
-        "Earth" to 1,
-        "Mars" to 2,
-        "Jupiter" to 79,
-        "Saturn" to 82,
-        "Uranus" to 27,
-        "Neptune" to 14
+
+    val cookies = listOf(
+        Cookie("Chocolate Chip", false, false, 1.69),
+        Cookie("Banana Walnut", true, false, 1.49),
+        Cookie("Vanilla Creme", false, true, 1.59),
+        Cookie("Chocolate Peanut Butter", false, true, 1.49),
+        Cookie("Snickerdoodle", true, false, 1.39),
+        Cookie("Blueberry Tart", true, true, 1.79),
+        Cookie("Sugar and Sprinkles", false, false, 1.39)
     )
 
-    solarSystem.forEach {
-        println("${it.key} has ${it.value} moons.")
+    val fullMenu = cookies.map {
+        "${it.name} - $${it.price}"
     }
+
+    fullMenu.forEach{
+        println(it)
+    }
+
 }
