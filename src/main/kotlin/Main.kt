3cf0e4fded0e481a6f99e5b7c28861e4576f277d
Glenn Halls <gjhalls@gmail.com>
2023-06-16 15:48:56 +1000

    List and MutableList

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,24 @@
 fun main() {
-    val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
-    val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
-    val solarSystem = rockPlanets + gasPlanets
-    println(solarSystem)
-    val newSolarSystem = solarSystem + "Pluto"
-    newSolarSystem.forEach { println(it) }
+    val solarSystem = listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    println(solarSystem.size)
+    println(solarSystem[2])
+    println(solarSystem.indexOf("Earth"))
+    println(solarSystem.indexOf("Pluto"))
+    println()
+    for (planet in solarSystem) {
+        println(planet)
+    }
+    println()
+    val newSolarSystem: MutableList<String> = mutableListOf()
+    solarSystem.forEach {
+        newSolarSystem.add(it)
+    }
+    newSolarSystem.add("Pluto")
+    newSolarSystem.add(3, "Theia")
+
+    newSolarSystem.forEach {
+        println(it)
+    }
+
+    newSolarSystem.removeAt(8)
 }
