a6880b2f357b1d733e1cc504d59ec2e046ec6f9d
Glenn Halls <gjhalls@gmail.com>
2023-06-16 14:30:19 +1000

    Extensions via Interfaces

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ enum class Difficulty {
     EASY, MEDIUM, HARD
 }
 
-class Quiz {
+class Quiz : ProgressPrintable {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false.", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
@@ -17,18 +17,23 @@ class Quiz {
         var total: Int = 10
         var answered: Int = 3
     }
+
+    override val progressText: String
+        get() = "$answered of $total answered."
+
+    override fun printProgressBar() {
+        repeat(Quiz.answered) { print("▓")}
+        repeat(Quiz.total - Quiz.answered) { print("▒")}
+        println()
+        println(progressText)
+    }
 }
 
-val Quiz.StudentProgress.progressText: String
-    get() = "$answered of $total answered"
-
-fun Quiz.StudentProgress.printProgressBar() {
-    repeat(Quiz.answered) { print("▓")}
-    repeat(Quiz.total - Quiz.answered) { print("▒")}
-    println()
-    println(Quiz.progressText)
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
 }
 
 fun main() {
-    Quiz.printProgressBar()
+    Quiz().printProgressBar()
 }
