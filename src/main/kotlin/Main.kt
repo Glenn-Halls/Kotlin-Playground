f52bfbb7d789881d5b67faeab01cc42d9d08ece1
Glenn Halls <gjhalls@gmail.com>
2023-06-16 18:47:37 +1000

    groupBy()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -25,12 +25,14 @@ fun main() {
         println(it)
     }
 
-    val softBakedMenu = cookies.filter { it.softBaked }
+    val groupedMenu = cookies.groupBy { it.softBaked }
+    val softBakedMenu = groupedMenu[true] ?: emptyList()
+    val crunchyMenu = groupedMenu[false] ?: emptyList()
 
     println()
-    println("Soft Cookies:")
-    softBakedMenu.forEach {
-        println("${it.name} - $${it.price}")
-    }
-
+    println("Soft cookies:")
+    softBakedMenu.forEach { println("${it.name} - $${it.price}") }
+    println()
+    println("Crunchy cookies:")
+    crunchyMenu.forEach { println("${it.name} - $${it.price}") }
 }
