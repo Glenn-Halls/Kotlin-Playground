5b69335076103f0865199a61b4ff14c2185f7279
Glenn Halls <gjhalls@gmail.com>
2023-06-16 14:58:01 +1000

    Arrays

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,60 +1,8 @@
-data class Question<T>(
-    val questionText: String,
-    val answer: T,
-    val difficulty: Difficulty
-)
-
-enum class Difficulty {
-    EASY, MEDIUM, HARD
-}
-
-class Quiz : ProgressPrintable {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
-    val question2 = Question<Boolean>("The sky is green. True or false.", false, Difficulty.EASY)
-    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
-
-    companion object StudentProgress {
-        var total: Int = 10
-        var answered: Int = 3
-    }
-
-    override val progressText: String
-        get() = "$answered of $total answered."
-
-    override fun printProgressBar() {
-        repeat(Quiz.answered) { print("▓")}
-        repeat(Quiz.total - Quiz.answered) { print("▒")}
-        println()
-        println(progressText)
-    }
-
-    fun printQuiz() {
-        question1.let {
-            println(it.questionText)
-            println(it.answer)
-            println(it.difficulty)
-            println()
-        }
-        question2.let {
-            println(it.questionText)
-            println(it.answer)
-            println(it.difficulty)
-            println()
-        }
-        question3.let {
-            println(it.questionText)
-            println(it.answer)
-            println(it.difficulty)
-            println()
-        }
-    }
-}
-
-interface ProgressPrintable {
-    val progressText: String
-    fun printProgressBar()
-}
-
 fun main() {
-    Quiz().apply { printQuiz() }
+    val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
+    val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
+    val solarSystem = rockPlanets + gasPlanets
+    println(solarSystem)
+    val newSolarSystem = solarSystem + "Pluto"
+    newSolarSystem.forEach { println(it) }
 }
