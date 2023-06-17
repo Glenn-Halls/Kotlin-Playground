266df8a98bb4de85fa2c7d56e68910b8f1158f3a
Glenn Halls <gjhalls@gmail.com>
2023-06-17 15:54:09 +1000

    Task 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,17 @@
 class Event (
     title: String,
     description: String? = null,
-    daypart: String,
+    daypart: Daypart,
     duration: Int
 ){}
 
+enum class Daypart {
+    MORNING,
+    AFTERNOON,
+    EVENING
+}
+
 fun main() {
-    val test = Event("Study Kotlin", "Commit to studying Kotlin at least 15 minutes per day.", "Evening", 15)
+    val test = Event("Study Kotlin", "Commit to studying Kotlin at least 15 minutes per day.", Daypart.EVENING, 15)
     println(test)
 }
