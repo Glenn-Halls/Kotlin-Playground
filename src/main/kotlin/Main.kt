3d7f7d27da0d9f4b340c150af545d682f41363e9
Glenn Halls <gjhalls@gmail.com>
2023-06-17 16:12:05 +1000

    Task 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
-class Event (
-    title: String,
-    description: String? = null,
-    daypart: Daypart,
-    durationInMinutes: Int
-){}
+data class Event (
+    val title: String,
+    val description: String? = null,
+    val daypart: Daypart,
+    val durationInMinutes: Int
+)
 
 enum class Daypart {
     MORNING,
@@ -18,8 +18,10 @@ val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, dura
 val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
 val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
 
-val events: MutableList<Event> = mutableListOf(event1, event2, event3, event4, event5, event6)
+val events = mutableListOf(event1, event2, event3, event4, event5, event6)
+val shortEvents = events.filter { it.durationInMinutes < 60 }
+
 
 fun main() {
-    println(events.size)
+    println("You have ${shortEvents.size} short events.")
 }
