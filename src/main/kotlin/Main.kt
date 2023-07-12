33b0092bf1d6b5ab5900ba29adce59be8485a86d
Glenn Halls <gjhalls@gmail.com>
2023-07-12 18:21:03 +1000

    Coroutine Dispatchers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,17 @@
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import kotlin.system.measureTimeMillis
+import kotlinx.coroutines.*
 
 fun main() {
-    val time = measureTimeMillis {
-        runBlocking {
-            println("Weather forecast")
-            println(getWeatherReport())
-            println("I think this will be second")
+    runBlocking {
+        println("${Thread.currentThread().name} - runBlocking function")
+        launch {
+            println("${Thread.currentThread().name} - launch function")
+            withContext(Dispatchers.Default) {
+                println("${Thread.currentThread().name} - withContext function")
+                delay(1000)
+                println("10 results found")
+            }
+            println("${Thread.currentThread().name} - end of launch function")
         }
+        println("Loading...")
     }
-    println("Execution time: ${time / 1000.0} seconds")
-}
-
-suspend fun getWeatherReport() = coroutineScope {
-    val forecast = async { getForecast() }
-    val temperature = async { getTemperature() }
-
-    delay(200)
-    temperature.cancel()
-    "${forecast.await()}"
-}
-
-suspend fun getForecast(): String {
-    delay(1000)
-    return "Sunny"
-}
-
-suspend fun getTemperature(): String {
-    delay(500)
-    return "30\u00b0C"
 }
